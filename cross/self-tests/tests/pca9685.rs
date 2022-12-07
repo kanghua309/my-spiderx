8a6768933e5b9ba0dc15995c92bf4279dd1d578e
king <kanghua151@msn.com>
2022-12-07 23:21:03 +0800

    ...

--- cross/self-tests/tests/pca9685.rs
+++ cross/self-tests/tests/pca9685.rs
@@ -44,12 +44,11 @@ mod tests {
     fn setup() -> State {
         defmt::println!("test init");
         let board = microbit::Board::take().unwrap();
-        let i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
-        let address = Address::default();//0b100_0000
-        let mut pwm = Pca9685::new(i2c, 0b111_0000).unwrap();
-
-        pwm.set_prescale(100).unwrap();
+        let i2c = { twim::Twim::new(board.TWIM0, board.i2c_external.into(), FREQUENCY_A::K100) };
+        //let address = Address::default();//0b100_0000
+        let mut pwm = Pca9685::new(i2c, 0x80).unwrap();
         pwm.enable().unwrap();
+        pwm.set_prescale(100).unwrap();
 
         let timer = Timer::new(board.TIMER0);
         State {pca:pwm,timer}
@@ -57,20 +56,42 @@ mod tests {
 
     #[test]
     fn simple_set_duty(state: &mut State) {
-        defmt::println!("simple_set_duty");
-        // Turn on channel 0 at 0
-        loop {
-            defmt::println!("loop");
-            let mut on = [10; 16];
-            let mut off = [3500; 16];
-            state.pca.set_all_on_off(&on, &off).expect("TODO: panic message");
-            state.timer.delay_ms(1000u32);
+            defmt::println!("simple_set_duty");
+            // Turn on channel 0 at 0
+            loop {
+                defmt::println!("loop");
+                let mut on = [10; 16];
+                let mut off = [3500; 16];
+                //state.pca.set_all_on_off(&on, &off).unwrap();
+                state.pca.set_channel_on_off(Channel::C1,0,3500);
+                state.timer.delay_ms(1000u32);
+            }
 
+            // pwm.set_channel_on(Channel::C0, 0).unwrap();
+            // // Turn off channel 0 at 2047, which is 50% in the range `[0..4095]`.
+            // pwm.set_channel_off(Channel::C0, 2047).unwrap();
+            //let _ = pwm.destroy();
         }
 
-        // pwm.set_channel_on(Channel::C0, 0).unwrap();
-        // // Turn off channel 0 at 2047, which is 50% in the range `[0..4095]`.
-        // pwm.set_channel_off(Channel::C0, 2047).unwrap();
-        //let _ = pwm.destroy();
-    }
+    //     defmt::println!("simple_set_duty");
+    //     // Turn on channel 0 at 0
+    //     state.pca.set_channel_on(Channel::All, 0).unwrap();
+    //     let servo_min = 90; // minimum pulse length (out of 4096)
+    //     let servo_max = 700; // maximum pulse length (out of 4096)
+    //     let mut current = servo_min;
+    //     let mut factor: i16 = 1;
+    //     loop {
+    //         defmt::println!("loop:{}",current);
+    //         state.pca.set_channel_off(Channel::All, current).unwrap();
+    //         //state.pca.set_channel_off(Channel::C1, servo_min + (servo_max - current))
+    //         //    .unwrap();
+    //         if current == servo_max {
+    //             factor = -1;
+    //         } else if current == servo_min {
+    //             factor = 1;
+    //         }
+    //         current = (current as i16 + factor) as u16;
+    //         state.timer.delay_ms(1000u32);
+    //     }
+    // }
 }
