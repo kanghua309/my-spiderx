6a6e084331b94c52cd5ac10b885f027eb5ae9b4e
king <kanghua151@msn.com>
2022-12-10 15:00:25 +0800

    can walk now in test

--- cross/self-tests/tests/pca9685/s90_driver.rs
+++ cross/self-tests/tests/pca9685/s90_driver.rs
@@ -13,6 +13,12 @@ struct State {
     timer:Timer<TIMER0>,
 }
 
+
+
+
+
+
+
 #[defmt_test::tests]
 mod tests {
     use core::time::Duration;
@@ -58,14 +64,14 @@ mod tests {
     //     let mut s90 = S90::new(&mut state.i2c,
     //                            0x80,
     //                            0,
-    //                            Degrees(90.0),
     //                            false).unwrap();
     //     loop {
+    //         s90.write(Degrees(0.0));
     //         defmt::println!("curent angle:{}",s90.read().0);
-    //         state.timer.delay_ms(1_0000_u16 as u32);
+    //         state.timer.delay_ms(1_000_u16 as u32);
     //         for index in 0..1 {
     //             defmt::println!("index:{}",index);
-    //             for angle in 0..180 {
+    //             for angle in 0..90 {
     //                 s90.write(Degrees(angle as f64));
     //                 state.timer.delay_ms(1_00_u16 as u32);
     //                 defmt::println!("curent angle:{}",s90.read().0)
@@ -100,43 +106,168 @@ mod tests {
     fn simple_animate(state: &mut State) {
         defmt::println!("simple_animate");
         // Turn on channel 0 at 0
-        let mut hip_rear_left = S90::new(& state.i2c,
-                               0x80,
-                               0,
-                               false).unwrap();
-
-        let mut knee_rear_left = S90::new(& state.i2c,
+        let mut hip_front_left = S90::new(& state.i2c,
                                          0x80,
-                                         15,
+                                         0,
                                          false).unwrap();
-        animate(
-            &mut [
-                Move::new(&mut hip_rear_left, 90.0.degrees()),
-                Move::new(&mut knee_rear_left, 0.0.degrees()),
-            ],
-            5000,
-            &mut state.timer,
-        );
-        loop {
-            defmt::println!("loop ... ");
+        let mut hip_front_right = S90::new(& state.i2c,
+                                          0x80,
+                                          1,
+                                          true).unwrap();
+        let mut hip_rear_left = S90::new(& state.i2c,
+                                           0x80,
+                                           2,
+                                           true).unwrap();
+        let mut hip_rear_right = S90::new(& state.i2c,
+                                         0x80,
+                                         3,
+                                         false).unwrap();
+
+        let mut knee_front_left = S90::new(& state.i2c,
+                                           0x80,
+                                           15,
+                                           false).unwrap();
+
+        let mut knee_front_right = S90::new(& state.i2c,
+                                           0x80,
+                                           14,
+                                           true).unwrap();
+        let mut knee_rear_left = S90::new(& state.i2c,
+                                            0x80,
+                                            13,
+                                            true).unwrap();
+        let mut knee_rear_right = S90::new(& state.i2c,
+                                          0x80,
+                                          12,
+                                          false).unwrap();
+
+        //init
+
+        hip_front_left.write(Degrees(45.0));
+        hip_front_right.write(Degrees(45.0));
+        hip_rear_left.write(Degrees(45.0));
+        hip_rear_right.write(Degrees(45.0));
+
+        knee_front_left.write(Degrees(135.0));
+        knee_front_right.write(Degrees(135.0));
+        knee_rear_left.write(Degrees(135.0));
+        knee_rear_right.write(Degrees(135.0));
+        // state.timer.delay_ms(1_000_u16 as u32);
+        // hip_rear_left.write(Degrees(0.0));
+        // state.timer.delay_ms(1_000_u16 as u32);
+        // animate(
+        //     &mut [
+        //         Move::new(&mut knee_rear_right, 180.0.degrees()),
+        //         //Move::new(&mut knee_rear_left, 0.0.degrees()),
+        //     ],
+        //     5000,
+        //     &mut state.timer,
+        // );
+        // loop {
+        //     defmt::println!("loop ... ");
+        //     animate(
+        //         &mut [
+        //             Move::new(&mut hip_rear_left, 135.0.degrees()),
+        //             Move::new(&mut knee_rear_left, 90.0.degrees()),
+        //         ],
+        //         5000,
+        //         &mut state.timer,
+        //     );
+        //     state.timer.delay_ms(1_000_u16 as u32);
+        //     animate(
+        //         &mut [
+        //             Move::new(&mut hip_rear_left, 90.0.degrees()),
+        //             Move::new(&mut knee_rear_left, 0.0.degrees()),
+        //         ],
+        //         5000,
+        //         &mut state.timer,
+        //     );
+        //     state.timer.delay_ms(1_000_u16 as u32);
+        // }
+        let speed = 5000;
+        for _ in 0..100 {
             animate(
                 &mut [
-                    Move::new(&mut hip_rear_left, 135.0.degrees()),
-                    Move::new(&mut knee_rear_left, 90.0.degrees()),
+                    Move::new(&mut knee_front_left, 100.0.degrees()),
+                    Move::new(&mut knee_rear_right, 100.0.degrees()),
                 ],
-                5000,
+                speed,
                 &mut state.timer,
             );
-            state.timer.delay_ms(1_000_u16 as u32);
             animate(
                 &mut [
-                    Move::new(&mut hip_rear_left, 90.0.degrees()),
-                    Move::new(&mut knee_rear_left, 0.0.degrees()),
+                    Move::new(&mut hip_front_left, 20.0.degrees()),
+                    Move::new(&mut hip_rear_right, 70.0.degrees()),
                 ],
-                5000,
+                speed,
                 &mut state.timer,
             );
-            state.timer.delay_ms(1_000_u16 as u32);
+            animate(
+                &mut [
+                    Move::new(&mut knee_front_left, 165.0.degrees()),
+                    Move::new(&mut knee_rear_right, 165.0.degrees()),
+                ],
+                speed,
+                &mut state.timer,
+            );
+            animate(
+                &mut [
+                    Move::new(&mut hip_front_left, 45.0.degrees()),
+                    Move::new(&mut hip_rear_right, 45.0.degrees()),
+                ],
+                speed,
+                &mut state.timer,
+            );
+            animate(
+                &mut [
+                    Move::new(&mut knee_front_left, 135.0.degrees()),
+                    Move::new(&mut knee_rear_right, 135.0.degrees()),
+                ],
+                speed,
+                &mut state.timer,
+            );
+            //**********
+            animate(
+                &mut [
+                    Move::new(&mut knee_front_right, 100.0.degrees()),
+                    Move::new(&mut knee_rear_left, 100.0.degrees()),
+                ],
+                speed,
+                &mut state.timer,
+            );
+            animate(
+                &mut [
+                    Move::new(&mut hip_front_right, 20.0.degrees()),
+                    Move::new(&mut hip_rear_left, 70.0.degrees()),
+                ],
+                speed,
+                &mut state.timer,
+            );
+
+            animate(
+                &mut [
+                    Move::new(&mut knee_front_right, 165.0.degrees()),
+                    Move::new(&mut knee_rear_left, 165.0.degrees()),
+                ],
+                speed,
+                &mut state.timer,
+            );
+            animate(
+                &mut [
+                    Move::new(&mut hip_front_right, 45.0.degrees()),
+                    Move::new(&mut hip_rear_left, 45.0.degrees()),
+                ],
+                speed,
+                &mut state.timer,
+            );
+            animate(
+                &mut [
+                    Move::new(&mut knee_front_right, 135.0.degrees()),
+                    Move::new(&mut knee_rear_left, 135.0.degrees()),
+                ],
+                speed,
+                &mut state.timer,
+            );
         }
     }
 }
